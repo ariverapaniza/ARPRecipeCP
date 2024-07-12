90c365fdd092946250f43f3b14e16cc1b307788f
ariverapaniza <ariverapaniza@gmail.com>
2024-07-12 10:34:54 +1000

    Loading View to try to make it work

---
+++ components/LoadingView.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+
+const LoadingView = ({ show }) => {
+  if (!show) return null;
+  
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#0000ff" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default LoadingView;
